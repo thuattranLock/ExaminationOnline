7bc1cacb20f3927a015170f57fe4985d584a824c
thuattranLock <thuat.how.to.code@gmail.com>
2020-11-18 10:57:47 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 //Database params
-define('DB_HOST', 'localhost'); 
-define('DB_USER', 'root'); 
-define('DB_PASS', 'thuat1810'); 
-define('DB_NAME', 'online_examination'); 
+define('DB_HOST', 'ec2-34-237-247-76.compute-1.amazonaws.com'); 
+define('DB_USER', 'wxrwwklfbsewuz'); 
+define('DB_PASS', 'f9dd54a7c3013878172afac4d5003768dce45fc42836a166e68c638a07aba011'); 
+define('DB_NAME', 'dc5ejpmffje6b8'); 
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT (Dynamic links)
-define('URLROOT', 'http://localhost/mvcPHP');
+define('URLROOT', 'https://examination-online.herokuapp.com');
 
 //Sitename
 define('SITENAME', 'MVC PHP');
